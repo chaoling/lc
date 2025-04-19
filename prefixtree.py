2dc50cea9f37be9191f2e5e7faa15f8bfb6bd0e2
Chao Ling <ling.chao@gmail.com>
2025-04-19 00:48:42 -0500

    solve lc implement Trie

---
+++ prefixtree.py
@@ -0,0 +1,44 @@
+class TrieNode:
+        def __init__(self):
+            self.is_end = False
+            self.children = {}
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+        self.is_end = False
+        
+
+    def insert(self, word: str) -> None:
+        curr = self.root
+        for ch in word:
+            if ch not in curr.children:
+                curr.children[ch] = TrieNode()
+            curr = curr.children[ch]
+        curr.is_end = True
+
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+        for ch in word:
+            if ch not in curr.children:
+                return False
+            curr = curr.children[ch]
+        return curr.is_end
+    
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.root
+        for ch in prefix:
+            if ch not in curr.children:
+                return False
+            curr = curr.children[ch]
+        return True
+        
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
