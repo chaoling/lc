56808f8bc6f47e3f8900f9305f1078be0ec02bac
Chao Ling <ling.chao@gmail.com>
2025-04-22 23:08:56 -0700

    solve lc 211 design and search words ds

---
+++ addnsearch.py
@@ -0,0 +1,83 @@
+'''
+211. Design Add and Search Words Data Structure
+
+Design a data structure that supports adding new words and finding if a string matches any previously added string.
+
+Implement the WordDictionary class:
+
+WordDictionary() Initializes the object.
+void addWord(word) Adds word to the data structure, it can be matched later.
+bool search(word) Returns true if there is any string in the data structure that matches word or false otherwise. word may contain dots '.' where dots can be matched with any letter.
+ 
+
+Example:
+
+Input
+["WordDictionary","addWord","addWord","addWord","search","search","search","search"]
+[[],["bad"],["dad"],["mad"],["pad"],["bad"],[".ad"],["b.."]]
+Output
+[null,null,null,null,false,true,true,true]
+
+Explanation
+WordDictionary wordDictionary = new WordDictionary();
+wordDictionary.addWord("bad");
+wordDictionary.addWord("dad");
+wordDictionary.addWord("mad");
+wordDictionary.search("pad"); // return False
+wordDictionary.search("bad"); // return True
+wordDictionary.search(".ad"); // return True
+wordDictionary.search("b.."); // return True
+ 
+
+Constraints:
+
+1 <= word.length <= 25
+word in addWord consists of lowercase English letters.
+word in search consist of '.' or lowercase English letters.
+There will be at most 2 dots in word for search queries.
+At most 104 calls will be made to addWord and search.
+'''
+
+class TrieNode:
+    def __init__(self):
+        self.is_end = False
+        self.children = {}
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+        
+
+    def addWord(self, word: str) -> None:
+        curr = self.root
+        for ch in word:
+            if ch not in curr.children:
+                curr.children[ch] = TrieNode()
+            curr = curr.children[ch]
+        curr.is_end = True
+        
+
+    def search(self, word: str) -> bool:
+         # inner helper closes over `self`
+        def _search_from(node: TrieNode, suffix: str) -> bool:
+            if not suffix:
+                return node.is_end
+
+            ch, rest = suffix[0], suffix[1:]
+            if ch == '.':
+                # try every possible path
+                return any(_search_from(child, rest)
+                           for child in node.children.values())
+            if ch in node.children:
+                return _search_from(node.children[ch], rest)
+            return False
+
+        return _search_from(self.root, word)
+        
+
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
