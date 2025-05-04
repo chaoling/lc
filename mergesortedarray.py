a3dec46260069c3ee8b3d1570e40358e46194c01
Chao Ling <ling.chao@gmail.com>
2025-05-04 12:38:33 -0700

    solve lc 88 merge sorted array in place

---
+++ mergesortedarray.py
@@ -0,0 +1,31 @@
+from typing import List
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        p1, p2, p = m - 1, n - 1, m + n - 1
+        while p2 >= 0:
+            if p1 >=0 and nums1[p1] > nums2[p2]:
+                nums1[p] = nums1[p1]
+                p1 -= 1
+            else:
+                nums1[p] = nums2[p2]
+                p2 -= 1
+            p -= 1
+
+if __name__ == "__main__":
+    sol = Solution()
+    nums1 = [1, 2, 3, 0, 0, 0]
+    m = 3
+    nums2 = [2, 5, 6]
+    n = 3
+    sol.merge(nums1, m, nums2, n)
+    print(nums1)  # Output should be [1, 2, 2, 3, 5, 6]
+    nums1 = [0]
+    m = 0
+    nums2 = [1]
+    n = 1
+    sol.merge(nums1, m, nums2, n)
+    assert nums1==[1], "error!"  # Output should be [1]
